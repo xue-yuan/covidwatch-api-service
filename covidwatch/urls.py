0950a7c82b9d7165c0f033ad16e9b532f391bbe0
xue-yuan <g00se.9527@gmail.com>
2020-07-20 01:20:10 +0800

    add

--- covidwatch/urls.py
+++ covidwatch/urls.py
@@ -17,9 +17,11 @@ from django.contrib import admin
 from django.urls import path, include
 
 from users.views import register_success
+from .views import index
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', index),
     path('user/', include('users.urls')),
     path('api/', include('users.urls')),
 ]
